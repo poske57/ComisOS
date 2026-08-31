ec826152970821f19c7ba0570015ceb8816a3763
poske57 <poske+github@ubukha.com>
2026-08-31 19:08:08 +0900

    feat: hand

--- comis/src/main.rs
+++ comis/src/main.rs
@@ -1,21 +1,45 @@
 mod settings;
 mod initialize;
 use dialoguer;
+use std::process::Command;
+use std::env;
 
 fn main() -> std::io::Result<()> {
     header();
     let settings = settings::Settings::load();
     initialize::initialize(&settings.repositories_path)?;
 
-    let choices = &["Chatを起動する", "設定を確認する", "巻き戻す"];
+    env::set_::current_dir(&settings.repositories_path)?;
+    let choices = &["Chatを起動する", "設定", "終了"];
     let choice: usize = dialoguer::Select::new()
     .items(choices)
     .default(0)
     .interact()?;
 
+    match choice {
+        0 => launch_chat(&settings.agent),
+        1 => config_menu(&settings),
+        _ => println!("終了します"),
+    }
+
     Ok(())
 }
 
 fn header() {
-    println!("{}", include_str!("../assets/logo.txt"));
+    println!("{}\n", include_str!("../assets/logo.txt"));
+}
+
+fn launch_chat(ai_agent: &String) {
+    println!("AI Agent を起動");
+    let status = Command::new(ai_agent)
+        .status();
+    if status.is_err() {
+        println!("AI Agent not found");
+    }
+}
+
+fn config_menu(&settings: settings::Settings) {
+    println!("Path    : {}", settings.repositories_path);
+    println!("AI agent: {}", settings.ai_agent);
+    println!("rebuild-command: {}", settings.rebuild_command);
 }
