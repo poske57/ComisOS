7e22c4712bd1b40594a0855d9c1ac8f89a234e99
poske57 <poske+github@ubukha.com>
2026-08-31 18:18:13 +0900

    feat: add logo

--- comis/src/main.rs
+++ comis/src/main.rs
@@ -1,8 +1,21 @@
 mod settings;
 mod initialize;
+use dialoguer;
 
 fn main() -> std::io::Result<()> {
+    header();
     let settings = settings::Settings::load();
     initialize::initialize(&settings.repositories_path)?;
+
+    let choices = &["Chatを起動する", "設定を確認する", "巻き戻す"];
+    let choice: usize = dialoguer::Select::new()
+    .items(choices)
+    .default(0)
+    .interact()?;
+
     Ok(())
+}
+
+fn header() {
+    println!("{}", include_str!("../assets/logo.txt"));
 }
